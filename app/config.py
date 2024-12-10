a788cf39bf9ee6dc1dae406a9671fc6dc1d585b1
Nick <unwrecker2@gmail.com>
2024-12-10 22:17:49 +0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ import os
 load_dotenv()
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATABASE_URL = os.path.join(BASE_DIR, 'todo.db')
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM", "HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
+SECRET_KEY = "your_secret_key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 MAX_FILE_SIZE = 2 * 1024 * 1024
