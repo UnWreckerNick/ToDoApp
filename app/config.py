bb4285f13d02097a49d9e41def68434e1d6d879d
Nick <unwrecker2@gmail.com>
2024-12-10 20:32:33 +0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -6,5 +6,5 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATABASE_URL = os.path.join(BASE_DIR, 'todo.db')
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)
 MAX_FILE_SIZE = 2 * 1024 * 1024
