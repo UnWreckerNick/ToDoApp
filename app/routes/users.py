ba543b5fc7761943c3bd46af7101e8e073aab95e
Nick <unwrecker2@gmail.com>
2024-12-12 15:27:45 +0700

    Add validator for username and password and DELETE for user

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
+from pydantic import ValidationError
 from app.schemas import RegisterUser
 from app.database import SessionLocal
 from app.auth import hash_password, create_access_token, pwd_context
@@ -9,14 +10,18 @@ router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.post("/register/")
 def register_user(user: RegisterUser, db: SessionLocal = Depends(get_db)):
-    if db.query(User).filter(User.username == user.username).first():
-        raise HTTPException(status_code=400, detail="Username already exists")
-    hashed_password = hash_password(user.password)
-    new_user = User(username=user.username, hashed_password=hashed_password)
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return {"message": "User registered successfully", "user_id": new_user.id}
+    try:
+        RegisterUser(**user.model_dump())
+        if db.query(User).filter(User.username == user.username).first():
+            raise HTTPException(status_code=400, detail="Username already exists")
+        hashed_password = hash_password(user.password)
+        new_user = User(username=user.username, hashed_password=hashed_password)
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+        return {"message": "User registered successfully", "user_id": new_user.id}
+    except ValidationError as ve:
+        raise HTTPException(status_code=400, detail=str(ve))
 
 @router.post("/login/")
 def login_user(user_login: RegisterUser, db: SessionLocal = Depends(get_db)):
@@ -25,3 +30,12 @@ def login_user(user_login: RegisterUser, db: SessionLocal = Depends(get_db)):
         raise HTTPException(status_code=401, detail="Invalid username and/or password")
     access_token = create_access_token(data={"sub": user.username})
     return {"access_token": access_token, "token_type": "bearer"}
+
+@router.delete("/{user_id}")
+def delete_user(user_id: int, db: SessionLocal = Depends(get_db)):
+    db_todo = db.query(User).filter(User.id == user_id).first()
+    if not db_todo:
+        raise HTTPException(status_code=404, detail="User not found!")
+    db.delete(db_todo)
+    db.commit()
+    return {"message": "User deleted"}
--- app/schemas.py
+++ app/schemas.py
@@ -1,10 +1,31 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from typing import Optional
 from datetime import datetime
+import re
 
 class RegisterUser(BaseModel):
     username: str
     password: str
+
+    @field_validator("username")
+    def validate_username_length(cls, value):
+        if len(value) < 3 or len(value) > 32:
+            raise ValueError("Username must be at least 3 characters length and less then 20")
+        return value
+
+    @field_validator("password")
+    def validate_password(cls, value):
+        if len(value) < 8 or len(value) > 32:
+            raise ValueError("Password must be at least 8 characters long.")
+        if not re.search(r'[A-Z]', value):
+            raise ValueError("Password must contain at least one uppercase letter.")
+        if not re.search(r'[a-z]', value):
+            raise ValueError("Password must contain at least one lowercase letter.")
+        if not re.search(r'\d', value):
+            raise ValueError("Password must contain at least one digit.")
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', value):
+            raise ValueError("Password must contain at least one special character.")
+        return value
 
 class TodoCreate(BaseModel):
     title: str
